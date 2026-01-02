9fdfee968019fbdfe37ed1726435481a49cab066
fauzanashshidiq <muhzeen07@gmail.com>
2026-01-02 16:35:35 +0700

    integrate pwa support

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('Aplikasi diperbarui. Refresh sekarang?')) {
+      updateSW(true)
+    }
+  },
+  onOfflineReady() {
+    console.log('Aplikasi siap digunakan offline!')
+  },
+})
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      devOptions: {
+        enabled: true,
+        type: 'classic',
+      navigateFallback: 'index.html',
+      },
+      manifest: {
+        name: 'Retro Pomodoro',
+        short_name: 'Pomodoro',
+        description: 'A Retro Themed Pomodoro Timer',
+        theme_color: '#efe7d8',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
